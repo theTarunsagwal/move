1f2a09c5987ef1f10d1f9f659325569ad935d98f
Tarun <tarunsagwal38@gmail.com>
2024-10-25 16:37:08 +0530

    new app

--- movies.js
+++ movies.js
@@ -4,6 +4,7 @@ let SEARCH="https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598
 let api = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1"
 let img="https://image.tmdb.org/t/p/w1280"
 app(api);
+
 async function app(a){
     const fet= await fetch(a)
     const res= await fet.json();
